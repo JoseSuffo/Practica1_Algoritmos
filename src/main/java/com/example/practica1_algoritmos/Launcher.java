3c9cf861dc7e5cfa0bb561a7985d0b85cbd16d4f
José Ramón Suffo Peimbert <suffo.jose@uabc.edu.mx>
2025-09-02 18:36:44 -0700

    Prueba

---
+++ src/main/java/com/example/practica1_algoritmos/Launcher.java
@@ -0,0 +1,9 @@
+package com.example.practica1_algoritmos;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
