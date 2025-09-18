8214e323bd916eee3e2970b4c27673cc689737dd
José Ramón Suffo Peimbert <suffo.jose@uabc.edu.mx>
2025-09-17 17:59:24 -0700

    Agregado de método para obtener el tamaño de una pila.

--- src/main/java/Pila/Pila.java
+++ src/main/java/Pila/Pila.java
@@ -39,9 +39,9 @@ public class Pila <T>{
     }
 
     public int getTamaño(){
-        int tamaño = 0;
-        for(int i = 0; i < pila.length; i++){
-            if(pila[i] != null){
+        int tamaño=0;
+        for(int i=0; i<pila.length; i++){
+            if(pila[i]!=null){
                 tamaño++;
             }
         }
